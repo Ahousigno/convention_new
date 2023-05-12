91170a18d6da1f9583acc9523875332d3a6c92fd
kolotcholoman.soro@uvci.edu.ci <kolotcholoman.soro@uvci.edu.ci>
2023-05-12 01:07:08 +0000

    modif

--- resources/views/client/presentation.blade.php
+++ resources/views/client/presentation.blade.php
@@ -5,19 +5,12 @@
 @section('contenu')
 <!-- Banner start -->
 
-@if (session('status'))
-<div align='center' class="alert alert-success">
-    {{ session('status') }}
-</div>
-@endif
-
-
-
 <?php $nav = "presentation" ?>
 <!-- Destinations -->
     <div class="container mt-5 mb-5" style="box-shadow: inset 0 0 0.5em blue;">
         <section class="our-webcoderskull padding-lg">
-            <div class="container">
+            <div class="container pt-3">
+                <a href="http://event.uvci.edu.ci/" class="blink" target="_blank">Plateforme Event</a>
                 <div class="row">
                     <h2 class="text-center mt-4 mb-5" style="color: #92278f; font-weight: bold">
                         Service Juridique, Communication et Partenariats
@@ -82,8 +75,25 @@
         </section>
     </div>
 
+    <style>
+        .blink {
+            animation: blinker 2s linear infinite;
+            background-color: rgb(18, 166, 80);
+            color: white;
+            padding: 10px;
+            border-radius: 12px;
+            font-weight: bold;
+        }
+        @keyframes blinker {
+            50% {
+                opacity: 0;
+            }
+        }
+        a:hover{
+            text-decoration: none;
+            color: white;
+            background-color: #92278f;
+        }
 
-<style>
-
-</style>
+    </style>
 @endsection()
